cf8135e69a0a0f9166ef9071f20294494031c0fa
Zachary Rose <zacerose@ut.utm.edu>
2023-01-19 13:27:00 -0600

    Class declarations complete, still needs implementation.

---
+++ PA-1-Abstract-Animal/World.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// defend design decision everything in same file
+namespace PA_1_Abstract_Animal
+{
+    abstract class Animal
+    {
+        private string Name, Noise;
+        private int Age, Weight;
+        public abstract void printInfo();
+        public abstract void makeNoise();
+        public abstract void ageUp();
+
+    }
+    class Cat : Animal
+    {
+        public override void printInfo()
+        {
+
+        }
+        public override void makeNoise()
+        {
+
+        }
+        public override void ageUp()
+        {
+
+        }
+    }
+    class Cassowary : Animal
+    {
+        public override void printInfo()
+        {
+
+        }
+        public override void makeNoise()
+        {
+
+        }
+        public override void ageUp()
+        {
+
+        }
+    }
+    class Cow : Animal
+    {
+        public override void printInfo()
+        {
+
+        }
+        public override void makeNoise()
+        {
+
+        }
+        public override void ageUp()
+        {
+
+        }
+    }
+    class World
+    {
+        List<Animal> roster;
+        static void Main(string[] args)
+        {
+            World zoo = new World();
+            zoo.roster = new List<Animal>();
+        }
+    }
+}
